c613645e9eecb8cf517fb6bf38f1bd9f6f4e8136
piyushmali1111 <piyushmali14126@gmail.com>
2026-06-23 16:46:11 +0530

    Implemented Basic RAG Pipeline

--- app.py
+++ app.py
@@ -1,18 +1,61 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI , GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
-from importlib.metadata import version
-from langchain_google_genai import ChatGoogleGenerativeAI
+user_query = input("Enter the query:")
 
-print(f"LangChain Core Version: {version('langchain-core')}")
-print(f"LangGraph Version: {version('langgraph')}")
-print(f"Google GenAI Version: {version('langchain-google-genai')}")
+persist_directory = 'db'
 
-def main():
-    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+prompt = PromptTemplate(
+   template = """
+   You are a really smart , helpful PDF chat assistant.
+   You will be provided with the User query and context from the pdf .
+   Your task is to generate Crisp and consise answers but in natural language with complete sentence.
+   If You dont know the answer or  context is not correclty provided then just return -
+   "Out of my Knowledge"
+    
+   User Question: 
+   {user_query},
+   
+   context:
+   {context}
 
-    response = llm.invoke("Say 'Setup complete' in one word")
-    print(response.content)
+""",
+input_variables= ['user_query', 'context']
 
-if __name__ == "__main__":
-    main()
+)
+
+chat_model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash', temperature= 0.5)
+embedding_model = GoogleGenerativeAIEmbeddings(model = "gemini-embedding-001")
+
+vector_store = Chroma(
+    persist_directory=persist_directory,
+    embedding_function= embedding_model,
+)
+
+
+retriever = vector_store.as_retriever(search_type = 'similarity',search_kwargs = {'k':5})
+
+searched_docs  = retriever.invoke(user_query)
+
+context = '\n\n'.join(
+    doc.page_content 
+    for doc in searched_docs
+    )
+
+output_parser = StrOutputParser()
+
+chain = prompt | chat_model | output_parser
+
+result = chain.invoke(
+    {'user_query':user_query,
+    'context':context}
+    )
+
+print(result)
