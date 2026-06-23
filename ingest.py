c0bcb77b3ab4414c7252fafc471e86aa5d6d15be
piyushmali1111 <piyushmali14126@gmail.com>
2026-06-23 15:21:42 +0530

    completed Data ingestion code - implemented embedding generation and vector store

--- ingest.py
+++ ingest.py
@@ -7,10 +7,41 @@ import os
 
 persist_directory = 'db'
 
+
 def main():
+    documents = []
     for root, dirs , files in os.walk('docs'):
         for file in files:
             if file.endswith('.pdf'):
                 print(file)
                 print(root)
-                Loader = PDFMinerLoader(os.path.join(root,file))
+                loader = PDFMinerLoader(os.path.join(root,file))
+                documents.extend(loader.load())
+    
+     #splitting the PDF documents into chunks
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200
+    )
+    
+    chunks = text_splitter.split_documents(documents)
+
+    # embeddings model 
+    embedding_model  = GoogleGenerativeAIEmbeddings(model = 'gemini-embedding-001') ## Currently Not specifying a Output Dimensionality
+    
+    vector_store = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    persist_directory=persist_directory,
+    client_settings = CHROMA_SETTINGS
+    )
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+
+
